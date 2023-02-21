66f2806ae055a7339bb293ce325743cdc9a96d4d
seleza1 <koposov.serezha@gmail.com>
2023-02-21 11:11:39 +0300

    success layout end get on the label F

--- Weather/HomeViewController.swift
+++ Weather/HomeViewController.swift
@@ -9,20 +9,43 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    @IBOutlet var temperatureLabel: UILabel!
-    
     private let viewModel = HomeViewModel()
+
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var feelsLikeLabel: UILabel!
+    @IBOutlet var minTemperatureLabel: UILabel!
+    @IBOutlet var maxTemperatureLabel: UILabel!
+    @IBOutlet var pressureLabel: UILabel!
+    @IBOutlet var humidityLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUi()
-        
-        NetworkController.fetchWeather { weather in
-            print(weather)
+        setupUI()
+
+
+        viewModel.fetchWeather { [weak self] in
+            DispatchQueue.main.async {
+                self?.setupUI()
+            }
         }
     }
 
-    private func setupUi() {
+    private func setupUI() {
+        setupHeader()
+        setupSubheader()
+    }
+
+    private func setupHeader() {
         temperatureLabel.text = viewModel.temperatureString
+        nameLabel.text = viewModel.nameString
+    }
+
+    private func setupSubheader() {
+        feelsLikeLabel.text = viewModel.feelsLikeTemperatureString
+        minTemperatureLabel.text = viewModel.minTemperatureString
+        maxTemperatureLabel.text = viewModel.maxTemperatureString
+        pressureLabel.text = viewModel.pressureString
+        humidityLabel.text = viewModel.humidityString
     }
 }
